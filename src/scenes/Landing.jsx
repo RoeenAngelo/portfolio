56d767f30bf70375994674f8c228653372ce2ec8
Roeen <roeenacal@gmail.com>
2023-09-23 01:29:42 -0700

    update landing page photo

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -15,22 +15,17 @@ const Landing = ({ setSelectedPage }) => {
       {/* IMAGE  */}
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
         {isAboveMediumScreens ? (
-          <img
-            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="./assets/royosemite.png"
-            alt="profile"
-          />
+          <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[300px]
+          before:w-5/6 before:max-w-[300px] md:before:max-w-[500px] before:h-5/6 before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="./assets/royosemite.png"
+              alt="profile"
+            />
+          </div>
         ) : (
-          // <div
-          //   className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-          // before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
-          // >
-          //   <img
-          //     className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-          //     src="./assets/royosemite.png"
-          //     alt="profile"
-          //   />
-          // </div>
           <img
             className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
             src="./assets/royosemite.png"
